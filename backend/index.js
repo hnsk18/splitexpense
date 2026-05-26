d1a1cf4e5545c5f45aae19fea911a16d570fa893
deeks528 <cvr23cseds@gmail.com>
2026-05-26 11:04:08 +0530

    added API docs and initialized backend

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://127.0.0.1:27017/expensedb')
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
